7f875c8d9be275dc99c3da804286209e78d1e2d7
Sashin <sashin_agrawal@epam.com>
2022-03-22 19:54:33 +0530

    first commit

--- src/com/leetcode/arrays/ZigZagConversion.java
+++ src/com/leetcode/arrays/ZigZagConversion.java
@@ -1,9 +1,62 @@
 package com.leetcode.arrays;
 
+import java.util.Arrays;
+
 public class ZigZagConversion {
 
 	public static void main(String[] args) {
-		String s = "EPAMISHIRING";
-		
+		String s = "PAYPALISHIRING";
+		System.out.println(convert(s, 3));
 	}
+	
+	 public static String convert(String str, int n) {
+	        
+	      if (n == 1)
+	        {
+	            return str;
+	        }
+	         char[] str1 = str.toCharArray();
+	 
+	        int len = str.length();
+	        
+	         String[] arr = new String[n];
+	        Arrays.fill(arr, "");
+	        
+	        int row = 0;
+	        boolean down = true; 
+	        
+	         for (int i = 0; i < len; ++i)
+	        {
+	          
+	            arr[row] += (str1[i]);
+	 
+	            if (row == n - 1)
+	            {
+	                down = false;
+	            }
+	             
+	            
+	            else if (row == 0)
+	            {
+	                down = true;
+	            }
+	 
+	          
+	            if (down)
+	            {
+	                row++;
+	            }
+	            else
+	            {
+	                row--;
+	            }
+	        }
+	 
+	       StringBuilder sb = new StringBuilder("");
+	        for (int i = 0; i < n; ++i)
+	        {
+	            sb.append(arr[i]);
+	        }
+	        return sb.toString();
+	    }
 }
