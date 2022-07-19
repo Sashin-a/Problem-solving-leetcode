d3565570b6ed2d5fac6d1705b1d1eb49812f5b81
Sashin <sashin_agrawal@epam.com>
2022-07-19 20:34:40 +0530

    Added parenthesis problem

---
+++ src/com/leetcode/arrays/ValidParenthesis.java
@@ -0,0 +1,35 @@
+package com.leetcode.arrays;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public boolean isValid(String s) {
+
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray())
+        {
+            if(map.containsKey(c))
+            {
+                stack.push(c);
+            }
+            else
+            {
+                if(!stack.isEmpty() && map.get(stack.peek()).equals(c))
+                {
+                    stack.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
