936f582c9a7053b3c85eeea9deb5250b66489ca8
Sashin <sashin_agrawal@epam.com>
2022-07-19 20:49:59 +0530

    Added BuyAndStockSell problem

---
+++ src/com/leetcode/arrays/BuyAndStockSell.java
@@ -0,0 +1,15 @@
+package com.leetcode.arrays;
+
+public class BuyAndStockSell {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+
+        for(int i=0; i<prices.length; i++)
+        {
+            max = Math.max(max, prices[i]-min);
+            min = Math.min(min, prices[i]);
+        }
+        return max;
+    }
+}
