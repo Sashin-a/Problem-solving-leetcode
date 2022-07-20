2634977d83567e3c1a831d0d916943e7a8de0d3c
Sashin <sashin_agrawal@epam.com>
2022-07-20 18:22:24 +0530

    LinkedList Cycle

---
+++ src/com/leetcode/arrays/DetectLinkedListCycle.java
@@ -0,0 +1,24 @@
+package com.leetcode.arrays;
+
+public class DetectLinkedListCycle {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(slow != null && fast !=null && fast.next !=null ){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) break;
+        }
+
+        if(fast == null || fast.next == null) return null;
+
+        fast = head;
+        while(fast != slow){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+}
