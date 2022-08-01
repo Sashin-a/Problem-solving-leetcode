083d645564b23cd416df8a51f188456ebc7352d6
Sashin <sashin_agrawal@epam.com>
2022-08-01 17:18:39 +0530

    Two pointers

---
+++ src/com/leetcode/twopointers/BuyAndStockSell.java
@@ -0,0 +1,15 @@
+package com.leetcode.twopointers;
+
+public class BuyAndStockSell {
+    public static int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+
+        for(int i=0; i<prices.length; i++)
+        {
+            max = Math.max(max, prices[i]-min);
+            min = Math.min(min, prices[i]);
+        }
+        return max;
+    }
+}
