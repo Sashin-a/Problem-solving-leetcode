c4f068c766e0636135d5e10f7ef5bd67dadf1093
Sashin <sashin_agrawal@epam.com>
2022-08-06 22:26:16 +0530

    phone Mnemonics

--- src/com/leetcode/string/PhoneMnemonics.java
+++ src/com/leetcode/string/PhoneMnemonics.java
@@ -1,2 +1,35 @@
-package com.leetcode.string;public class PhoneMnemonics {
+package com.leetcode.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneMnemonics {
+    private static List<String> phoneKeyMapping = List.of(
+            "0", "1", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+    );
+
+    public static void main(String[] args) {
+        PhoneMnemonics phoneMnemonics = new PhoneMnemonics();
+        System.out.println(phoneMnemonics.letterCombinations(""));
+    }
+
+    public List<String> letterCombinations(String digits) {
+        char[] partialMnemonics = new char[digits.length()];
+        List<String> mnemonics = new ArrayList<>();
+        phoneMnemonicHelper(partialMnemonics, mnemonics, 0, digits);
+        return mnemonics;
+    }
+
+    private void phoneMnemonicHelper(char[] partialMnemonics, List<String> mnemonics, int digit, String phoneNumber) {
+        if (digit == phoneNumber.length()) {
+            if (partialMnemonics.length > 0) {
+                mnemonics.add(new String(partialMnemonics));
+            }
+        } else {
+            for (char c : phoneKeyMapping.get(phoneNumber.charAt(digit) - '0').toCharArray()) {
+                partialMnemonics[digit] = c;
+                phoneMnemonicHelper(partialMnemonics, mnemonics, digit + 1, phoneNumber);
+            }
+        }
+    }
 }
